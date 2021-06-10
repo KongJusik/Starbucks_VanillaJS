3db57cb4bafdf6286409c343ff393a88fd3de349
KongJusik <kongjusik@naver.com>
2021-06-11 01:02:13 +0900

    Setting up footer section

--- scripts/main.js
+++ scripts/main.js
@@ -117,3 +117,6 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, "show")
     .addTo(new ScrollMagic.Controller());
 });
+
+const thisYear = document.querySelector(".this-year");
+thisYear.textContent = new Date().getFullYear();
